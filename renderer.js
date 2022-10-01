27fdd61b553cd559fc0b5614bb61ba8ba245b6f3
Deivid Almeida <mr.davialmeida@hotmail.com>
2022-09-30 21:20:49 -0300

    :tada: frist commit

---
+++ renderer.js
@@ -0,0 +1,7 @@
+const formLogin = document.querySelector("#form-login");
+formLogin.addEventListener("submit", (event) => {
+  event.preventDefault();
+for (const iterator of formLogin.ele) {
+  
+}
+});
