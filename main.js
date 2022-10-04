2f0876d227bc0270fbbc815af08a74149c12eaca
Deivid Almeida <mr.davialmeida@hotmail.com>
2022-10-04 18:04:21 -0300

    :sparkles: main page

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-// Modules to control application life and create native browser window
 const {app, BrowserWindow, ipcMain, dialog } = require("electron");
 const path = require("path");
 const Services  = require("./src/services/services");
 var Service = new Services();
+var mainWindow;
 
 function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     fullscreen:false,
     icon: "./src/assets/icon.png",
     webPreferences: {
@@ -19,39 +18,25 @@ function createWindow () {
 
   mainWindow.maximize();
 
-  // and load the index.html of the app.
-  mainWindow.loadFile("index.html")
+  mainWindow.loadFile("./src/pages/login/login.html")
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
   app.on("activate", function () {
-    // On macOS it"s common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it"s common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on("window-all-closed", function () {
   if (process.platform !== "darwin") app.quit()
 })
-
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
 
 ipcMain.on("submitForm", async function (event, data) {
   const response =  await Service.Auth(data);
   if(response.status > 299) 
     return dialog.showErrorBox("Acesso negado", "Credenciais inválidas");
-    console.log(response);
+  mainWindow.loadFile("./src/pages/main/main.html")
 })
